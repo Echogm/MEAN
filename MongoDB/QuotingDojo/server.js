b338f710ba84a56531d2f358c35fc58e71cc9195
eduardo <silverk2112@gmail.com>
2017-12-05 13:15:57 -0500

    Commit

--- MongoDB/QuotingDojo/server.js
+++ MongoDB/QuotingDojo/server.js
@@ -1,62 +1,50 @@
-// Require the Express Module
 var express = require('express');
-// Create an Express App
 var app = express();
-var mongoose = require("mongoose")
-// Require body-parser (to receive post data from clients)
-var bodyParser = require('body-parser');
-// Integrate body-parser with our App
-app.use(bodyParser.urlencoded({ extended: true }));
-// Require path
+var mongoose = require('mongoose');
+var bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true}));
 var path = require('path');
-// Setting our Static Folder Directory
 app.use(express.static(path.join(__dirname, './static')));
-// Setting our Views Folder Directory
 app.set('views', path.join(__dirname, './views'));
-// Setting our View Engine set to EJS
 app.set('view engine', 'ejs');
-
-mongoose.connect('mongodb://localhost/basic_mongoose');
-var UserSchema = new mongoose.Schema({
-    name: String,
-    age: Number
+mongoose.connect('mongodb://localhost/quoting_dojo');
+var QuoteSchema = new mongoose.Schema({
+	name: String,
+	quote: String
 });
-mongoose.model('User', UserSchema);
-var User = mongoose.model('User')
+
+mongoose.model('Quote', QuoteSchema);
+var Quote = mongoose.model('Quote');
 mongoose.Promise = global.Promise;
-// Routes
-// Root Request
-app.get('/', function(req, res) {
-    User.find({}, function(err, user){
-        if (err){
-            console.log(err)
-        }else{
-            console.log(user)
-            res.render('index', {user});
-        }
-    });
-});
-app.get('/users',function(req, res) {
-    res.render('user')
-})
-// Add User Request
-app.post('/users', function(req, res) {
-  console.log("POST DATA", req.body);
-  // create a new User with the name and age corresponding to those from req.body
-  var user = new User({name: req.body.name, age: req.body.age});
-  // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
-  user.save(function(err) {
-    // if there is an error console.log that something went wrong!
-    if(err) {
-      console.log('something went wrong');
-    } else { // else console.log that we did well and then redirect to the root route
-      console.log('successfully added a user!');
-      res.redirect('/');
-    }
-  })
-})
 
-// Setting our Server to Listen on Port: 8000
+app.get('/', function(req, res) {
+	res.render('index')
+
+});
+
+app.post('/new', function(req, res) {
+	console.log("POST DATA", req.body);
+	var quote = new Quote({name: req.body.name, quote: req.body.quote});
+	quote.save(function(err){
+		if (err) {
+			console.log('Something went wrong!')
+		}else {
+			console.log('successfully added a user!')
+			res.redirect('/quotes')
+		}
+	})
+});
+
+app.get('/quotes', function(req, res){
+	Quote.find({},function(err, quotes){
+		if (err) {
+			console.log(err);
+		}
+		console.log(quotes);
+		res.render('quotes', {quotes: quotes});
+	})
+});
+
 app.listen(8000, function() {
-    console.log("listening on port 8000");
-})
+	console.log("listening on port 8000");
+});
